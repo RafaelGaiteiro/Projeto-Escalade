549b2161d19ba8505920c9388f4bc32cf209b9cd
Rafael Krämer da Silva <rafaelkramerdasilva@gmail.com>
2023-02-24 16:12:57 -0300

    .

---
+++ JogoDosNumeros/js/script.js
@@ -0,0 +1,51 @@
+console.log("Jogo dos Números");
+
+var numInput = document.querySelector("#numSecreto");
+var entrada = numInput.textContent;
+var resultado;
+var exibeResultado = document.querySelector("#exibeResultado");
+
+btnAddNum = document.querySelector("#btn-addNum");
+
+btnAddNum.addEventListener("click", function (event) {
+  event.preventDefault();
+  //verificacao(resultado);
+  var numSecreto = Math.floor(Math.random() * 100);
+
+  if (entrada > numSecreto) {
+    console.log("O número digitado é maior que o número secreto.");
+    resultado = "O número digitado é maior que o número secreto.";
+  }
+  else if (entrada < numSecreto) {
+    console.log("O número digitado é menor que o número secreto.");
+    resultado = "O número digitado é menor que o número secreto.";
+  }
+  else if (entrada == numSecreto) {
+    console.log("Parabéns! Você acertou o número secreto!");
+    resultado = "Parabéns! Você acertou o número secreto!";
+  }
+
+
+
+  exibeResultado.textContent = resultado;
+});
+
+//var numSecreto = Math.floor(Math.random() * 100);
+
+/*
+function verificacao(entrada, numSecreto, resultado) {
+  if (entrada > numSecreto) {
+    console.log("O número digitado é maior que o número secreto.");
+    resultado = "O número digitado é maior que o número secreto.";
+  }
+  else if (entrada < numSecreto) {
+    console.log("O número digitado é menor que o número secreto.");
+    resultado = "O número digitado é menor que o número secreto.";
+  }
+  else if (entrada == numSecreto) {
+    console.log("Parabéns! Você acertou o número secreto!");
+    resultado = "Parabéns! Você acertou o número secreto!";
+  }
+  return resultado;
+}
+*/
