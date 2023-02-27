0981cbfc88ecd29e34b512cce0d0f0fbb8f33a98
Rafael Krämer da Silva <rafaelkramerdasilva@gmail.com>
2023-02-27 15:50:48 -0300

    Update filtra.js

--- Calculo IMC/js/filtra.js
+++ Calculo IMC/js/filtra.js
@@ -1,9 +1,21 @@
+// traz o input #filtrar-tabela e atribui ao campoFiltro
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
+// adiciona um escutador ao input, que recebe o tipo de evento
+// como input. Dessa forma todas vez que ouver um input(entrada de dado)
+// ele executa a função anônima
 campoFiltro.addEventListener("input", function() {
+    // traz os pacientes da tr .paciente e atribui a var pacientes
     var pacientes = document.querySelectorAll(".paciente");
 
+    // se o tamanho do valor/conteúdo do campoFiltro for maior que 0, então...
+    // observação: a propriedade this faz referência ao campoFiltro, traduzindo
+    // signfica "esse", pois estamos indicando ao quando o escutador
+    // do evento está atrelado
     if (this.value.length > 0) {
+        // laço de repetição
+        // inicia em 0 e percorre toda a extensão do pacientes
+        // serve filtrar todos
         for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
