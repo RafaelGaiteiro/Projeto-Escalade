856ea4a215411fa2d163c2d508cb35b381ed9fe2
Rafael Krämer da Silva <rafaelkramerdasilva@gmail.com>
2023-02-27 17:49:22 -0300

    Update buscar-pacientes.js

--- Calculo IMC/js/buscar-pacientes.js
+++ Calculo IMC/js/buscar-pacientes.js
@@ -1,10 +1,18 @@
+// traz o botão do HTML
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
+// evento de click
 botaoAdicionar.addEventListener("click", function() {
+    // atribui a var xhr o construtor
+    // o new XMLHttpRequest() é responsável por fazer uma requisição
+    // das informações de uma API, por exemplo
     var xhr = new XMLHttpRequest();
 
+    // link
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
+    // escutador
+    // quando houver o evento carregar
     xhr.addEventListener("load", function() {
         var erroAjax = document.querySelector("#erro-ajax");
 
