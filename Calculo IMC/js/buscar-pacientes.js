29a1d1f36a078060895f1bf22b6cc65e5c9d5da8
Rafael Krämer da Silva <rafaelkramerdasilva@gmail.com>
2023-02-28 12:45:07 -0300

    Update buscar-pacientes.js

--- Calculo IMC/js/buscar-pacientes.js
+++ Calculo IMC/js/buscar-pacientes.js
@@ -8,26 +8,33 @@ botaoAdicionar.addEventListener("click", function() {
     // das informações de uma API, por exemplo
     var xhr = new XMLHttpRequest();
 
-    // link
+    // xhr.open é onde escrevemos o link
+    // metodo GET para pegar as informações
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     // escutador
     // quando houver o evento carregar
     xhr.addEventListener("load", function() {
+        // busca o campo no qual é impresso o erro no HTML
         var erroAjax = document.querySelector("#erro-ajax");
 
+        // se o status do xhr for igual a 200 então está tudo OKAY
         if (xhr.status == 200) {
+            // portanto é aplicada a classe invisível no erro
             erroAjax.classList.add("invisivel");
+
             var resposta = xhr.responseText;
             var pacientes = JSON.parse(resposta);
 
             pacientes.forEach(function(paciente) {
                 adicionaPacienteNaTabela(paciente);
             });
+            // caso o status seja outro ele remove a classe invisível
+            // informando que a um erro
         } else {
             erroAjax.classList.remove("invisivel");
         }
     });
-
+// envia o xhr
     xhr.send();
 });
